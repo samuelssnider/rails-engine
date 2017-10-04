de05963658aeb2fa4c745335815cf5c874b54b8f
samuelssnider <sam_redins@yahoo.com>
2017-10-04 17:47:14 -0600

    six more tests passing

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -13,7 +13,7 @@ module Api
 				private
 				
 				def search_params
-					params.permit(:first_name, :last_name, :created_at, :updated_at)
+					params.permit(:id, :first_name, :last_name, :created_at, :updated_at)
 				end
 			end
 		end
--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -13,7 +13,7 @@ module Api
 				private
 				
 				def search_params
-					params.permit(:name, :description, :unit_price, :merchant_id,
+					params.permit(:id, :name, :description, :unit_price, :merchant_id,
 						            :created_at, :updated_at)
 				end
 			end
