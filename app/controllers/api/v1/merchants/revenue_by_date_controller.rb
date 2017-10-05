bfccb916db12bb7ef95b8eb17146433431239853
samuelssnider <sam_redins@yahoo.com>
2017-10-05 15:07:37 -0600

    one more green test

--- app/controllers/api/v1/merchants/revenue_by_date_controller.rb
+++ app/controllers/api/v1/merchants/revenue_by_date_controller.rb
@@ -4,7 +4,7 @@ module Api
 			class RevenueByDateController < ApplicationController
 				def index
 					merchant = Merchant.find(params[:merchant_id])
-					render json: {revenue: merchant.total_revenue}
+					render json: {revenue: (merchant.total_revenue.to_f/100.0).to_s}
 				end
 			end
 		end
