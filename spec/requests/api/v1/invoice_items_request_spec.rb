79656931c8d49a425201af4a704c7c3a0771a4d1
samuelssnider <sam_redins@yahoo.com>
2017-10-06 10:20:04 -0600

    invoice items request spec

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe 'Invoice Items API' do
+	include Helper
 	it "sends a list of Invoice Items" do
 		m = create(:merchant)
 		c = create(:customer)
@@ -45,6 +46,30 @@ describe 'Invoice Items API' do
 		invoice_item = JSON.parse(response.body)
 		
 		expect(invoice_item).not_to be_empty
+		expect(response).to have_http_status(200)
+	end
+	
+	it 'finds a invoice item' do
+		create_stuff
+		ii = InvoiceItem.last
+		
+		get "/api/v1/invoice_items/find?id=#{ii.id}"
+		
+		invoice_item = JSON.parse(response.body)
+		
+		expect(invoice_item).to have_value(1)
+		expect(response).to have_http_status(200)
+	end
+	it 'finds multiple invoice items' do
+		create_stuff
+		ii = InvoiceItem.last
+		
+		get "/api/v1/invoice_items/find_all?quantity=#{ii.quantity}"
+		
+		invoice_item = JSON.parse(response.body)
+		
+		expect(invoice_item.first).to have_value(1)
+		expect(invoice_item.count).to eq(3)
 		expect(response).to have_http_status(200)
 	end
 end
